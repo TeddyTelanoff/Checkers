3ba0e7f625dec2437273498af17149ddd5b7d5bf
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2021-01-25 09:46:36 -0800

    Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance { get; private set; } = null;
+	public static Vector2 Start { get => Instance.m_Start; }
+	public static Vector2 End { get => Instance.m_End; }
+	public static Vector2 BoardSpaces { get => Instance.m_BoardSpaces; }
+
+	public Vector2 m_Start;
+	public Vector2 m_End;
+	public Vector2Int m_BoardSpaces;
+
+    private void Awake()
+    {
+		Debug.Assert(Instance == null);
+		Instance = this;
+    }
+}
--- Assets/Scripts/Selection.cs
+++ Assets/Scripts/Selection.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Selection : MonoBehaviour
 {
-	public Vector2 m_Start;
-	public Vector2 m_End;
 	public Vector2Int m_Position;
-	public Vector2Int m_Board;
 	public float m_Y;
 
 	private Vector2 m_SquareOff;
@@ -15,8 +12,8 @@ public class Selection : MonoBehaviour
 
 	private void Start()
 	{
-		float blockSzX = (m_End.x - m_Start.x) / m_Board.x;
-		float blockSzY = (m_End.y - m_Start.y) / m_Board.y;
+		float blockSzX = (GameManager.End.x - GameManager.Start.x) / GameManager.BoardSpaces.x;
+		float blockSzY = (GameManager.End.y - GameManager.Start.y) / GameManager.BoardSpaces.y;
 		m_BlockSize = new Vector2(blockSzX, blockSzY);
 
 		transform.localScale = new Vector3(m_BlockSize.x / 10, 1, m_BlockSize.y / 10);
@@ -26,8 +23,8 @@ public class Selection : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		float x = m_BlockSize.x * m_Position.x + m_Start.x + m_SquareOff.x;
-		float y = m_BlockSize.y * m_Position.y + m_Start.y + m_SquareOff.y;
+		float x = m_BlockSize.x * m_Position.x + GameManager.Start.x + m_SquareOff.x;
+		float y = m_BlockSize.y * m_Position.y + GameManager.Start.y + m_SquareOff.y;
 
 		transform.position = new Vector3(x, m_Y, y);
 	}
