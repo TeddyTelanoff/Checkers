d38c5f3a20115a0fa3b605e124cacedb10767d17
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2021-01-25 09:58:57 -0800

    color

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,14 +8,21 @@ public class GameManager : MonoBehaviour
 	public static Vector2 Start { get => Instance.m_Start; }
 	public static Vector2 End { get => Instance.m_End; }
 	public static Vector2 BoardSpaces { get => Instance.m_BoardSpaces; }
+	public static Vector2 SquareSize { get => Instance.m_SquareSize; }
 
 	public Vector2 m_Start;
 	public Vector2 m_End;
 	public Vector2Int m_BoardSpaces;
 
-    private void Awake()
-    {
+	private Vector2 m_SquareSize;
+
+	private void Awake()
+	{
 		Debug.Assert(Instance == null);
 		Instance = this;
-    }
+
+		float blockSzX = (m_End.x - m_Start.x) / m_BoardSpaces.x;
+		float blockSzY = (m_End.y - m_Start.y) / m_BoardSpaces.y;
+		m_SquareSize = new Vector2(blockSzX, blockSzY);
+	}
 }
--- Assets/Scripts/Selection.cs
+++ Assets/Scripts/Selection.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class Selection : MonoBehaviour
 {
+	public Color m_NormalColor;
+	public Color m_OverColor;
+
 	public Vector2Int m_Position;
 	public float m_Y;
 
 	private Vector2 m_SquareOff;
-	private Vector2 m_BlockSize;
 
-	private void Start()
+	private void Awake()
 	{
-		float blockSzX = (GameManager.End.x - GameManager.Start.x) / GameManager.BoardSpaces.x;
-		float blockSzY = (GameManager.End.y - GameManager.Start.y) / GameManager.BoardSpaces.y;
-		m_BlockSize = new Vector2(blockSzX, blockSzY);
+		GetComponent<MeshRenderer>().material.color = m_NormalColor;
+		transform.localScale = new Vector3(GameManager.SquareSize.x / 10, 1, GameManager.SquareSize.y / 10);
 
-		transform.localScale = new Vector3(m_BlockSize.x / 10, 1, m_BlockSize.y / 10);
-
-		m_SquareOff = m_BlockSize / 2;
+		m_SquareOff = GameManager.SquareSize / 2;
 	}
 
 	private void FixedUpdate()
 	{
-		float x = m_BlockSize.x * m_Position.x + GameManager.Start.x + m_SquareOff.x;
-		float y = m_BlockSize.y * m_Position.y + GameManager.Start.y + m_SquareOff.y;
+		float x = GameManager.SquareSize.x * m_Position.x + GameManager.Start.x + m_SquareOff.x;
+		float y = GameManager.SquareSize.y * m_Position.y + GameManager.Start.y + m_SquareOff.y;
 
 		transform.position = new Vector3(x, m_Y, y);
+	}
+
+	private void OnMouseEnter()
+	{
+		GetComponent<MeshRenderer>().material.color = m_OverColor;
+	}
+
+	private void OnMouseExit()
+	{
+		GetComponent<MeshRenderer>().material.color = m_NormalColor;
 	}
 }
